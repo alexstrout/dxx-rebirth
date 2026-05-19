alexstrout/dxx-rebirth

commit 5e1d664d57bd6ceae906470c8b9b88ee7cda8ad0

gamecontroller: remove write-only `GC_Joystick`
`joy.cpp` has a read-write static global `Joystick` that is used to
remember whether a hat is up or down, so that duplicate events can be
ignored.  `gamecontroller.cpp` was copied from `joy.cpp` and added a
corresponding `GC_Joystick`, but `GC_Joystick` is write-only.  Remove
it.

common/arch/sdl/gamecontroller.cpp
@@ -71,13 +71,6 @@ struct d_gamecontroller
 
 static std::array<d_gamecontroller, DXX_MAX_JOYSTICKS> GameControllers;
 
-// Virtual joystick state
-static struct gc_joyinfo {
-#if DXX_MAX_BUTTONS_PER_JOYSTICK
-	std::array<uint8_t, JOY_MAX_BUTTONS> button_state{};
-#endif
-} GC_Joystick;
-
 struct d_event_joystickbutton : d_event
 {
 	const unsigned button;
@@ -353,9 +346,6 @@ void gamecontroller_flush()
 {
 	if (!num_controllers)
 		return;
-#if DXX_MAX_BUTTONS_PER_JOYSTICK
-	GC_Joystick.button_state = {};
-#endif
 	gc_axis_values = {};
 }
 
@@ -366,8 +356,6 @@ window_event_result gc_button_handler(const SDL_ControllerButtonEvent *const cbe
 	const unsigned button = cbe->button;
 	if (button >= GC_NUM_BUTTONS)
 		return window_event_result::ignored;
-
-	GC_Joystick.button_state[button] = cbe->state;
 
 	const d_event_joystickbutton event{
 		(cbe->type == SDL_CONTROLLERBUTTONDOWN) ? event_type::joystick_button_down : event_type::joystick_button_up,
@@ -382,7 +370,6 @@ namespace {
 
 static window_event_result gc_send_axis_button_event(unsigned button, event_type e)
 {
-	GC_Joystick.button_state[button] = (e == event_type::joystick_button_up) ? 0 : 1;
 	const d_event_joystickbutton event{e, button};
 	return event_send(event);
 }

commit ad021d5bd7d742bec12df8c05604be5d7dc587ab

gamecontroller: remove initialization of `GC_Joystick`
`GC_Joystick` was a write-only variable, and all other uses of it were
removed in commit 5e1d664d57bd6ceae906470c8b9b88ee7cda8ad0.  This use
either was not removed, or the removal was lost in a rebase resolution.
It is not needed.  Remove it.

Fixes: 5e1d664d57bd6ceae906470c8b9b88ee7cda8ad0 ("gamecontroller: remove write-only `GC_Joystick`")

common/arch/sdl/gamecontroller.cpp
@@ -297,7 +297,6 @@ void gamecontroller_init()
 		return;
 	}
 
-	GC_Joystick = {};
 	gc_axis_values = {};
 	gcbutton_text.clear();
 	num_controllers = 0;