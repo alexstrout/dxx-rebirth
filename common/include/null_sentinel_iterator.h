f2baec8d2b17c53fd427ce48a08b7be672d69997
Kp <3164941+vLKp@users.noreply.github.com>
2026-05-24 23:26:51 +0000

    Document null_sentinel_iterator

--- common/include/null_sentinel_iterator.h
+++ common/include/null_sentinel_iterator.h
@@ -2,11 +2,27 @@
 
 #include <iterator>
 
+/* `null_sentinel_sentinel` is an empty type returned by the `end()` method of
+ * containers that use `null_sentinel_iterator` to traverse their storage.
+ * `null_sentinel_iterator` defines `operator==` to recognize
+ * `null_sentinel_sentinel` and respond appropriately.
+ *
+ * The template parameter exists solely to prevent accidental mixing of
+ * sentinels from different container types.
+ */
 template <typename I>
 struct null_sentinel_sentinel
 {
 };
 
+/* `null_sentinel_iterator` wraps a pointer of type `I *` and allows the caller
+ * to traverse a contiguous container of indeterminate length.  The container
+ * is required to have an element with value `nullptr` as a sentinel, placed
+ * after all valid elements.  Any elements beyond the sentinel are assumed to
+ * be outside the container's storage, and are not accessed.
+ * `null_sentinel_iterator` compares equal to `null_sentinel_sentinel` when the
+ * `null_sentinel_iterator` reaches the `nullptr` value in the container.
+ */
 template <typename I>
 class null_sentinel_iterator
 {
@@ -18,7 +34,7 @@ public:
 	using reference = I &;
 	constexpr null_sentinel_iterator() = default;
 	constexpr null_sentinel_iterator(const pointer i) :
-		p(i)
+		p{i}
 	{
 	}
 	pointer get() const
@@ -36,7 +52,7 @@ public:
 	}
 	null_sentinel_iterator operator++(int)
 	{
-		auto r = *this;
+		auto r{*this};
 		++ *this;
 		return r;
 	}
@@ -49,13 +65,18 @@ private:
 	pointer p{nullptr};
 };
 
+/* `null_sentinel_array` wraps an array of pointers and treats the array as a
+ * container suitable for use with `null_sentinel_iterator`.
+ *
+ * `null_sentinel_array` does not take ownership of the array.
+ */
 template <typename I>
 class null_sentinel_array
 {
 	I *const b;
 public:
 	constexpr null_sentinel_array(I *const i) :
-		b(i)
+		b{i}
 	{
 	}
 	null_sentinel_iterator<I> begin() const
